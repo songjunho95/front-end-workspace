21f608cb0681c5101d8c679af5134dd9b75f3b26
songjunho95 <songpro3290@naver.com>
2024-10-07 11:40:54 +0900

    123

--- 4_jQuery/05_효과.js
+++ 4_jQuery/05_효과.js
@@ -1,23 +1,23 @@
 // 1. show, hide, toggle
 $("#show").click(() => {
-    $("#img").show(3000);
+  $("#img").show(3000);
 });
 $("#hide").click(() => {
-    $("#img").hide(3000);
+  $("#img").hide(3000);
 });
 $("#toggle").click(() => {
-    $("#img").toggle(3000);
-})
+  $("#img").toggle(3000);
+});
 
 // 2. fadeIn, fadeOut, fadeToggle, fadeTo
 $("#fadeIn").click(() => {
-    $("#img").fadeIn(3000);
+  $("#img").fadeIn(3000);
 });
 $("#fadeOut").click(() => {
-    $("img").fadeOut(3000);
+  $("img").fadeOut(3000);
 });
 $("#fadeToggle").click(() => {
-    $("#img").fadeToggle(3000);
+  $("#img").fadeToggle(3000);
 });
 $("#img").hover(
   function () {
@@ -25,31 +25,23 @@ $("#img").hover(
   }, // mouseenter
   function () {
     $(this).fadeTo(500, 1);
-
-  }  // mouseleave
+  } // mouseleave
 );
 
 // 3. slideDown, slideUp, slideToggle
 $(".menu").click((e) => {
-    let content = $(e.target).next();
-    /*
+  let content = $(e.target).next();
+  /*
     if(content.css("display") === "none") {
         content.slideDown(500);
     } else {
         content.slideUp(500);
     }*/
-   // content.slideToggle(500);
+  // content.slideToggle(500);
 
-   // 하나의 콘텐츠만 slideDown!
-   $(".contents").slideUp();
-   if (content.css("display") === "none") {
-    content.slideDown(500);
-   }
+  // 하나의 콘텐츠만 slideDown!
+  $(".contents").slideUp();
+  if (content.css("display") === "none") {
+    content.slidDown(500);
+  }
 });
-
-
-
-
-
-
-
